9b480a5cddd6559ce7faadf2822038bc14170171
Daniil Pazin <52215503+danpazin@users.noreply.github.com>
2026-04-04 21:23:26 +0200

    Add Uniforms struct definition and header guard in Uniforms.h

---
+++ Mir/Sources/Mir/Shaders/Uniforms.h
@@ -0,0 +1,15 @@
+// Uniforms.h
+// Mir
+//
+// SPDX-License-Identifier: Apache-2.0
+// Copyright © 2026 Daniil Pazin. All rights reserved.
+//
+
+#ifndef Uniforms_h
+#define Uniforms_h
+
+typedef struct {
+    
+} Uniforms;
+
+#endif /* Uniforms_h */
