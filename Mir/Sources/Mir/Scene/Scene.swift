79dcab43b761dbb49178fe1144c997a3a568b068
Daniil Pazin <52215503+danpazin@users.noreply.github.com>
2026-04-15 17:04:56 +0200

    Add Scene struct with default camera initialization

--- Mir/Sources/Mir/Scene/Scene.swift
+++ Mir/Sources/Mir/Scene/Scene.swift
@@ -1,8 +1,27 @@
-// File.swift
+// Scene.swift
 // Mir
 //
 // SPDX-License-Identifier: Apache-2.0
 // Copyright © 2026 Daniil Pazin. All rights reserved.
 //
 
-import Foundation
+struct Scene {
+
+    // MARK: - Properties
+
+    var camera: Camera
+
+    // MARK: - Initializers
+
+    init() {
+        camera = Camera(
+            position: [0, 0, 3],
+            target: [0, 0, 0],
+            up: [0, 1, 0],
+            fov: (Float.pi / 4),
+            near: 0.01,
+            far: 100,
+            aspectRatio: 1
+        )
+    }
+}
